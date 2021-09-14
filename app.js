07a15d992ee92279050c3d43c622680c2d55da05
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-09-14 17:31:05 +0800

    created js script to add habits

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 var habitInput = document.querySelector('#habitInput');
 var addHabitBtn = document.querySelector('#addHabitBtn');
+var showDate =document.querySelector('#showDate');
+var displayHabits = document.querySelector('#displayHabits');
 
+// created date object with constructor
+
+var today = new Date();
+// designed a var to give today's date and month has index value, so solved that by adding plus 1
+
+var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
 var habitStorage = [];
+
+showDate.innerText = date;
+
+function addHabit(){
+    var newHabit = document.createElement('p')
+    var habitText = document.createTextNode(habitInput.value);
+    newHabit.appendChild(habitText);
+    displayHabits.appendChild(newHabit);
+    habitStorage.push(habitInput.value);
+    
+
+}
+addHabitBtn.addEventListener('click', addHabit)
+
+
+
