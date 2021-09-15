0bb1b0f0bfe8edf9a116c3fcf579f5d378f0a730
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-09-15 13:57:05 +0800

    fix: changed function to add habits in object

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-var habitInput = document.querySelector('#habitInput');
-var addHabitBtn = document.querySelector('#addHabitBtn');
-var showDate =document.querySelector('#showDate');
-var displayHabits = document.querySelector('#displayHabits');
+let habitInput = document.querySelector('#habitInput');
+let addHabitBtn = document.querySelector('#addHabitBtn');
+let showDate =document.querySelector('#showDate');
+let displayHabits = document.querySelector('#displayHabits');
 
 // created date object with constructor
 
-var today = new Date();
-// designed a var to give today's date and month has index value, so solved that by adding plus 1
+let today = new Date();
+// designed a let to give today's date and month has index value, so solved that by adding plus 1
 
-var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-var habitStorage = [];
+let date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+let habitStorage = [];
 
-showDate.innerText = date;
 
-function addHabit(){
-    var newHabit = document.createElement('p')
-    var habitText = document.createTextNode(habitInput.value);
-    newHabit.appendChild(habitText);
-    displayHabits.appendChild(newHabit);
-    habitStorage.push(habitInput.value);
+// showDate.innerText = date;
+
+// function addHabit(){
+//     let newHabit = document.createElement('p')
+//     let habitText = document.createTextNode(habitInput.value);
+//     newHabit.appendChild(habitText);
+//     displayHabits.appendChild(newHabit);
+//     habitStorage.push(habitInput.value);
     
 
+// }
+
+//CRUD Functions
+function addHabit(){
+    habitStorage.push({name: habitInput.value,
+    date: Date.now()});        
 }
+// Update function
+//takes the habit to update and new habit
+// find the old habit index and replace it with new one.
+//
+
+console.log(habitStorage);
 addHabitBtn.addEventListener('click', addHabit)
 
-
-
+// functional tasks
+// UI
