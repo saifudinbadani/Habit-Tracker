ae37948e5a3e1f045d0f40241b1820e0f28077f7
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-10-12 20:12:41 +0800

    close button

--- app.js
+++ app.js
@@ -1,57 +1,45 @@
 let habitParentList = document.querySelector('#habitList')
 let habitList = document.getElementsByTagName('li');
 let habitInput = document.querySelector('#habitInput');
-
-
 let addBtn = document.querySelector('#addButton');
+let closeBtn = document.getElementsByClassName('close')
 
 
-function createHabit(){
+for (i = 0; i < habitList.length; i++) {
+    let close = document.createElement('span');
+    let closeText = document.createTextNode('\u00D7');
+    close.className = 'close'
+    close.appendChild(closeText);
+    habitList[i].appendChild(close);
+}
+
+function createHabit() {
     let addHabitToList = document.createElement('li');
     let captureText = document.createTextNode(habitInput.value);
     addHabitToList.appendChild(captureText);
-    if(habitInput.value === ''){
+    if (habitInput.value === '') {
         alert('Write something')
-    }
-    else{
+    } else {
         habitParentList.appendChild(addHabitToList);
         habitInput.value = '';
+
+        //close  button
+        let close = document.createElement('span');
+        let closeText = document.createTextNode('\u00D7');
+        close.className = 'close'
+        close.appendChild(closeText);
+        addHabitToList.appendChild(close);  
     }
-    
+
 }
 
+// function removeHabit(){
+//     for(i=0; i < habitList.length; i++){
+//         habitList[i].style.display = 'none'
+//     }
+// }
+
+// closeBtn.addEventListener("click", removeHabit);
+
 addBtn.addEventListener("click", createHabit)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// created date object with constructor
-
-// let today = new Date();
-
-// designed a let to give today's date and month has index value, so solved that by adding plus 1
-
-// todaysDate.innerText = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-// let habitStorage = [];
-
-
 
