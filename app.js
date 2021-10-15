1e0a94b64858de68704222eaebb114bc6201da58
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-10-15 16:41:01 +0800

    Added remove functionality and edit button

--- app.js
+++ app.js
@@ -3,14 +3,41 @@ let habitList = document.getElementsByTagName('li');
 let habitInput = document.querySelector('#habitInput');
 let addBtn = document.querySelector('#addButton');
 let closeBtn = document.getElementsByClassName('close')
+let editBtn = document.getElementsByClassName('edit')
 
-
+// add close button when DOM is loaded
 for (i = 0; i < habitList.length; i++) {
     let close = document.createElement('span');
     let closeText = document.createTextNode('\u00D7');
     close.className = 'close'
     close.appendChild(closeText);
     habitList[i].appendChild(close);
+}
+
+// remove habit functionality added when DOM is loaded
+for (i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].addEventListener('click', function removeHabit() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    })
+}
+
+
+// add pencil icon when DOM is loaded
+for (i = 0; i < habitList.length; i++) {
+    let edit = document.createElement('span');
+    let editText = document.createTextNode('\u270E');
+    edit.className = 'edit'
+    edit.appendChild(editText);
+    habitList[i].appendChild(edit);
+}
+
+// edit functionality when DOM is loaded
+for (i = 0; i < editBtn.length; i++) {
+    editBtn[i].addEventListener('click', function editHabit() {
+        var div = this.parentElement;
+        div.innerText=habitInput.value;
+    })
 }
 
 function createHabit() {
@@ -28,18 +55,28 @@ function createHabit() {
         let closeText = document.createTextNode('\u00D7');
         close.className = 'close'
         close.appendChild(closeText);
-        addHabitToList.appendChild(close);  
-    }
+        addHabitToList.appendChild(close);
 
+        //remove functionality when a new habit is added
+        for (i = 0; i < closeBtn.length; i++) {
+            closeBtn[i].addEventListener('click', function removeHabit() {
+                var div = this.parentElement;
+                div.style.display = "none";
+            })
+        }
+
+        // add pencil icon when a new habit is added
+        for (i = 0; i < habitList.length; i++) {
+            let edit = document.createElement('span');
+            let editText = document.createTextNode('\u270E');
+            edit.className = 'edit'
+            edit.appendChild(editText);
+            habitList[i].appendChild(edit);
+        }
+    }
 }
 
-// function removeHabit(){
-//     for(i=0; i < habitList.length; i++){
-//         habitList[i].style.display = 'none'
-//     }
-// }
 
-// closeBtn.addEventListener("click", removeHabit);
+
 
 addBtn.addEventListener("click", createHabit)
-
